119eea04ee5082034fbc49bba0767a6335305fa2
Davin <bdv111@snu.ac.kr>
2020-03-29 15:55:53 +0900

    Django

---
+++ django/hantijae_bookstore/books/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from books.models import Book, Category, Author
+from books.serializers import BookSerializer
+
+class BookViewSet(viewsets.GenericViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        return Response(self.get_serializer(queryset, many=True).data)
+
+    def retrieve(self, request, pk=None):
+        book = get_object_or_404(Book, pk=pk)
+        return Response(self.get_serializer(book).data)
