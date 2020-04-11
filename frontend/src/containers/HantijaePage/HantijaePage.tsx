9dadbd795078984364f48677e3924fb4fd13c7ae
Davin <bdv111@snu.ac.kr>
2020-04-12 06:02:32 +0900

    hantijae page

--- frontend/src/containers/HantijaePage/HantijaePage.tsx
+++ frontend/src/containers/HantijaePage/HantijaePage.tsx
@@ -3,99 +3,50 @@ import { connect } from 'react-redux';
 
 import { bookActions } from '../../store/actions';
 import './HantijaePage.css';
-import {
-  BookCountWithCart,
-} from '../../components';
 
 interface Props {
   location: any;
-  book: any;
   getBookStatus: string;
   onGetBook: (id: number) => any;
   history: any;
 }
 
-interface State {
-  book: any;
-}
-
-class HantijaePage extends Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      book: {},
-    };
-  }
-
+class HantijaePage extends Component<Props> {
   componentDidMount() {
     window.scrollTo(0, 0);
-    this.props.onGetBook(this.props.location.pathname.split('=')[1])
-      .then(() => this.setState({ book: this.props.book }));
   }
 
   render() {
-    /* eslint-disable-next-line */
-    img = <img src={require(`./kiki.jpeg`)} />;
-
-
-    const { book } = this.props;
-
     return (
       <div className="HantijaePage">
-        <div className="BookDetailUpper">
-          <div className="BookCoverStand">
-            {img}
-          </div>
-          <div className="BookDetailInfo">
-            <h1 id="title">{book.title}</h1>
-            <h1 id="subtitle">{book.subtitle}</h1>
-            <h3 id="authors">{authorStr}</h3>
-            <div className="BookDetailInfoList">
-              <div className="InfoItem">
-                <div className="LItem">가격</div>
-                <div className="RItem">
-                  {book.full_price}
-                  원
-                </div>
-              </div>
-              <div className="InfoItem">
-                <div className="LItem">발행일</div>
-                <div className="RItem">{book.published_date}</div>
-              </div>
-              <div className="InfoItem">
-                <div className="LItem">
-                  쪽수
-                </div>
-                <div className="RItem">
-                  {book.page_count}
-                  쪽
-                </div>
-              </div>
-              <div className="InfoItem">
-                <div className="LItem">
-                  판형
-                </div>
-                <div className="RItem">
-                  {book.size}
-                </div>
-              </div>
-              <div className="InfoItem">
-                <div className="LItem">
-                  ISBN
-                </div>
-                <div className="RItem">
-                  {book.isbn}
-                </div>
-              </div>
-              {bookCart}
-            </div>
+        <div className="HantijaeUpper">
+          <h1 className="HantijaeTitle">도서출판 한티재는...</h1>
+          <div className="HantijaeImage">
+            {/* eslint-disable-next-line */}
+            <img src={require(`./kiki.jpeg`)} />
           </div>
         </div>
-        <div className="BookDescriptions">
+        <div className="HantijaeDescription">
           <p>
-            {desc}
+            도서출판 한티재는 2010년 대구에서 문을 연 작은 출판사입니다.
+            사람과 사람, 사람과 자연, 오늘 세대와 내일 세대가 공존하는 사회에 보탬이 되는 책을 만들기 위해 노력하고 있습니다.
           </p>
         </div>
+        <hr className="HantijaeCenterLine" />
+        <h3 className="EventTitle">
+          한티재 10주년 기념 ‘10권 10만 원 책바구니’ 특판 이벤트를 진행하고 있습니다!
+        </h3>
+        <p className="EventDescription">
+          <li>
+            책의 정가와 상관 없이 한티재 책 최대 10권을 10만 원에 구입하실 수 있습니다. 단, 10권보다 적게 담거나, 정가 총액이 10만 원보다 적어도 결제 금액은 10만 원입니다^^
+          </li>
+          <li>
+            특판 이벤트에서 2020년 올해 나온 신간은 제외됩니다.
+          </li>
+          <li>
+            한티재 온라인 책창고를 둘러보시고 책바구니에 책을 담아주세요. 지난 10년 동안 한티재는 총 108종의 단행본을 출간했습니다(절판 12종 포함).
+          </li>
+        </p>
       </div>
     );
   }
