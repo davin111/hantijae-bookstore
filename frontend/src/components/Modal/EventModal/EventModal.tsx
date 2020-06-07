dc1c7d5d44e92d3c6140ee0ae297af1a467ca691
Davin <bdv111@snu.ac.kr>
2020-06-07 17:41:33 +0900

    fix minor design

--- frontend/src/components/Modal/EventModal/EventModal.tsx
+++ frontend/src/components/Modal/EventModal/EventModal.tsx
@@ -87,16 +87,27 @@ class EventModal extends Component<Props, State> {
         <Modal.Body>
           <p className="EventDescriptionModal">
             따뜻한 의사 김동은 선생님의 첫 책 출간을 기념하여
+            <br />
             저자 자필 사인본 이벤트를 준비했습니다.
+            <br />
             한티재 온라인 책창고에서 책을 주문하시면,
+            <br />
             힘들고 아픈 사람 곁에 늘 함께 있는 선생님의
+            <br />
             따뜻한 글씨를 책에 담아 보내드립니다.
+            <br />
             많은 신청 부탁드립니다!
           </p>
+          <hr className="CenterLineModal" />
           <h3 className="NewBookTitles">
             『당신이 나의 백신입니다』
             <br />
             ― 감염병과 혐오의 시대, 의사 김동은이 만난 아름다운 사람들
+          </h3>
+          <h3 className="NewBookInfos">
+            김동은 지음
+            <br />
+            (2020년 6월 22일 출간 | 15,000원)
           </h3>
           <div className="NewBookStandModal">
             {/* eslint-disable-next-line */}
@@ -112,11 +123,6 @@ class EventModal extends Component<Props, State> {
             >
               바로가기
             </Button>
-            <h3 className="NewBookInfos">
-              김동은 지음
-              <br />
-              (2020년 6월 22일 출간, 15,000원)
-            </h3>
             <p className="EventDescriptionModal">
               ♥ 이벤트 기간 : 2020년 6월 8일(월) ~ 6월 20일(토)
               <br />
@@ -124,17 +130,13 @@ class EventModal extends Component<Props, State> {
               <br />
               ♥ 이벤트 참가 방법
               <br />
-              1. 한티재 온라인 책창고에서 회원 가입을 합니다.
+              &nbsp;&nbsp;1. 한티재 온라인 책창고에서 회원 가입을 합니다.
               <br />
-              2. 『당신이 나의 백신입니다』를 주문합니다.
+              &nbsp;&nbsp;2. 『당신이 나의 백신입니다』를 주문합니다.
               <br />
-              3. 주문 단계에서, 사인 받을 분의 이름을 확인하여 정확히 입력해주세요.
+              &nbsp;&nbsp;3. 주문 단계에서, 사인 받을 분의 이름을 확인하여 정확히 입력해주세요.
               <br />
-              &nbsp;&nbsp;- 여러 명일 경우 모두 적어주시면 됩니다.
-              <br />
-              &nbsp;&nbsp;- 여러 곳으로 보내야 할 경우 담당자에게 메일이나 전화로 문의해 주세요.
-              <br />
-              4. ‘주문 완료’ 버튼을 누르고 결제해주세요. 결제가 확인되어야 발송됩니다.
+              &nbsp;&nbsp;4. ‘주문 완료’ 버튼을 누르고 결제해주세요. 결제가 확인되어야 발송됩니다.
               <br />
               ♥ 문의 : 053-743-8368 | hantibooks@gmail.com
             </p>
