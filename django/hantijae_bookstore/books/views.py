ddb5b2666909ccf5fe6662a67d08abdb02bcd349
Davin <bdv111@snu.ac.kr>
2021-06-20 00:45:34 +0900

    get book count & new book cover imgs

--- django/hantijae_bookstore/books/views.py
+++ django/hantijae_bookstore/books/views.py
@@ -2,13 +2,13 @@ from django.core.cache import cache
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
-from rest_framework import status, viewsets
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from books.models import Book, Category, Author, Series
+from books.models import Book, Category, Series
 from books.serializers import (BookSerializer, CategorySerializer, SeriesSerializer,
-    SimpleBookSerializer, SimpleSeriesSerializer)
+                               SimpleBookSerializer, SimpleSeriesSerializer)
 
 
 class BookViewSet(viewsets.GenericViewSet):
@@ -38,6 +38,14 @@ class BookViewSet(viewsets.GenericViewSet):
         book = get_object_or_404(Book, pk=pk)
         return Response(self.get_serializer(book).data)
 
+    @action(detail=False, methods=["GET"])
+    def count(self, request):
+        book_count = cache.get("book_count")
+        if book_count is None:
+            book_count = self.get_queryset().count()
+            cache.set("book_count", book_count, 3600)
+        return Response({"count": book_count})
+
 
 class CategoryViewSet(viewsets.GenericViewSet):
     queryset = Category.objects.all()
@@ -53,13 +61,13 @@ class SeriesViewSet(viewsets.GenericViewSet):
     serializer_class = SimpleSeriesSerializer
 
     def get_serializer_class(self):
-        if self.action == 'retrieve':
+        if self.action == "retrieve":
             return SeriesSerializer
         return self.serializer_class
 
     def list(self, request):
-        data = cache.get('series')
-        if not data:
+        data = cache.get("series")
+        if data is None:
             queryset = self.get_queryset()
             fixed_tuple = ("단행본", "교양문고", "산문선", "시선", "팸플릿")
             data = []
@@ -69,13 +77,13 @@ class SeriesViewSet(viewsets.GenericViewSet):
                     data.append(self.get_serializer(series).data)
                 except ObjectDoesNotExist:
                     pass
-            cache.set('series', data, 3600)
+            cache.set("series", data, 3600)
         return Response(data)
 
     def retrieve(self, request, pk=None):
         series = get_object_or_404(Series, pk=pk)
-        data = cache.get(f'series_{pk}')
-        if not data:
+        data = cache.get(f"series_{pk}")
+        if data is None:
             data = self.get_serializer(series).data
-            cache.set(f'series_{pk}', data, 3600)
+            cache.set(f"series_{pk}", data, 3600)
         return Response(data)
