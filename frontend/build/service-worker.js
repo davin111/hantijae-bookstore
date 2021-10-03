79c4cd0249b17780ccc28edd2446fe4db88eed15
Davin <bdv111@snu.ac.kr>
2021-10-03 22:11:44 +0900

    build

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83da3424c9bf98d478841fa26ee9744b.js"
+  "/precache-manifest.3825a0844f4fe72b458ca2a447f5fc7a.js"
 );
 
 self.addEventListener('message', (event) => {
